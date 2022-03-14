7da559b8cc2a51738e4aa6f2ca5481051ee41dad
Gitsukru <suisse1022@gmail.com>
2022-03-14 11:07:56 +0100

    first ccommit

---
+++ exemple.py
@@ -0,0 +1,43 @@
+star = "✪"
+with open('test.txt', 'w', encoding="UTF-8") as f:
+    f.write(f"\n{star}")
+
+# message = '\"Hello, my name is Nico."\"Today is a beatiful day!"\"I\'d like to eat an apple."\
+#'
+#print(message)
+
+
+
+#exemple de variables python
+
+#startHour = "13:30 AM"
+#birthday_date = "07.08.1975"
+# _color = "Sky blue"
+
+#print (startHour + " " + birthday_date + " " + _color)
+
+# 13:30 AM 07.08.1975 Sky blue
+
+
+#result = 4 + 5 * 3
+#print(result)
+
+#result = (4 + 5) * 3
+#print(result)
+
+
+#name = "Rick"
+#greetings = "Hello"
+#space = " "
+#message = greetings + space + name
+#print(message)
+
+
+#degre celsius
+celsius = "é"  
+# degre farenheit  
+farenheit = (celsius * 1.8) +32 
+
+print (farenheit)
+
+
